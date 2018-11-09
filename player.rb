92665ef408814af350d2476b9f2473263463e16d
Kevin Peery <kevin.peery@gmail.com>
2018-11-09 15:57:24 -0700

    improvements to player.rb Person class

--- player.rb
+++ player.rb
@@ -13,10 +13,14 @@ class Person
     #if the answer is greater than 5000, then set their balance to 5000
     if input > 5000
       puts "\nThat's a bit too much money to gamble with. Maximum allowed is $5,000."
+      #this is telling the Ruby program to wait 5 sec before moving to the next line of code.
+      sleep 5
       @balance = 5000
+      puts "\n We're limiting your balance to #{@balance}. Your change returned is #{input - @balance}."
       #otherwise, the balance is their answer provided.
     else
       @balance = input
+      puts "You're playing with $#{@balance}."
     end
   end
 end
