fdfe046f162392e4442f8f13232aa750cc099127
Kevin Peery <kevin.peery@gmail.com>
2018-11-09 16:25:24 -0700

    improvements to player.rb Person class

--- casino.rb
+++ casino.rb
@@ -1,8 +1,16 @@
+require "pry"
+
 class Casino
   attr_accessor :name, :balance
 
   def initialize(name, balance)
     @name = name
     @balance = balance
+  end
+
+  def exit_program
+    binding.pry
+    puts "Thanks for playing! Bye!"
+    exit(0)
   end
 end
--- player.rb
+++ player.rb
@@ -1,7 +1,16 @@
-class Person
+require "pry"
+require_relative "./casino"
+
+class Person < Casino
   attr_accessor :name, :balance
 
   def initialize
+    #initialize balance and set it to zero.
+    @balance = 0
+    get_user_info
+  end
+
+  def get_user_info
     # `` execute the shell command in ruby. The below clears the terminal.
     puts `clear`
     puts "Welcome! Let's get you signed up for a game!"
@@ -9,18 +18,20 @@ class Person
     @name = gets.strip
     puts `clear`
     puts "How much money are you playing with today?"
+    #any string will be zero by default when you call to_i on it.
     input = gets.strip.to_i
     #if the answer is greater than 5000, then set their balance to 5000
     if input > 5000
       puts "\nThat's a bit too much money to gamble with. Maximum allowed is $5,000."
-      #this is telling the Ruby program to wait 5 sec before moving to the next line of code.
-      sleep 5
+      #this is telling the Ruby program to wait 3.5 sec before moving to the next line of code.
+      sleep 3.5
       @balance = 5000
       puts "\n We're limiting your balance to #{@balance}. Your change returned is #{input - @balance}."
-      #otherwise, the balance is their answer provided.
     else
-      @balance = input
-      puts "You're playing with $#{@balance}."
+      puts "Please come back when you have some money to play with."
+      exit_program
     end
   end
 end
+
+Person.new
