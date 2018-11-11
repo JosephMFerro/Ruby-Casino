0e367497db3e452f12f38c6bedc7b0e4af6158c9
Kevin Peery <kevin.peery@gmail.com>
2018-11-10 17:34:26 -0700

    edit player feature

--- player.rb
+++ player.rb
@@ -1,10 +1,10 @@
 require_relative "./wallet"
-require_relative "./welcome"
+# require_relative "./welcome"
 
-class Person < Wallet
-  attr_accessor :name, :balance
+class Person
+  attr_accessor
 
-  def initialize(name, balance)
+  def initialize
     #initialize balance and set it to zero.
     @name = nil
     super()
@@ -26,8 +26,7 @@ class Person < Wallet
     if @name.match(/[a-zA-Z]{3,}/)
       puts
       puts "Hello #{@name}, how much money are you playing with today? Note: Our max is $5,000."
-      get_money
-      casino = Casino.new
+      get_balance
     else
       puts "Please provide a valid name meeting the following criteria: upper (A-Z) or lowercase (a-z) letters greater than 3 characters total. Let's try again."
       puts
@@ -36,24 +35,27 @@ class Person < Wallet
   end
 
   def get_balance
+    puts
     puts "How much money are you playing with today?"
     #any string will be zero by default when you call to_i on it.
     input = gets.strip.to_i
-    #if the answer is greater than 5000, then set their balance to 5000
     @balance = input
-    puts "#{@name}, you're playing with $#{@balance}. Have fun!"
-    if @balance > 5000
+    if @balance == 0
+      puts "Please enter an integer and/or valid amount to start your balance with."
+      @balance = gets.strip.to_i
+      #"ABC".to_i returns 0
+      #if the answer is greater than 5000, then set their balance to 5000
+    elsif @balance > 5000
       puts "\nThat's a bit too much money to gamble with. Maximum allowed is $5,000."
       #this is telling the Ruby program to wait 3.5 sec before moving to the next line of code.
       sleep 3.5
       @balance = 5000
-      puts "\n We're limiting your balance to #{@balance}. Your change returned is #{input - @balance}."
-    elsif @balance == 0
-      puts "Please come back when you have some money to play with."
-      exit_program
+      puts "\n We're limiting your balance to $#{@balance}. Your change returned is $#{input - @balance}."
     else
-      #send to next method...place to go.
-      main_menu
+      @balance = sprintf("%.2f", @balance)
+      puts "#{@name}, you're playing with $#{@balance}. Have fun!"
     end
   end
 end
+
+Person.new
